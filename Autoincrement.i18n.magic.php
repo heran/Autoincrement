9f1f4c4065adffbd9fef72c03469dedfc4915d9c
Heran <choudouhu@163.com>
2015-10-17 14:05:10 +0800

    Create Autoincrement.i18n.magic.php

---
+++ Autoincrement.i18n.magic.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Internationalisation file for Autoincrement extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$magicWords = array();
+
+/** English (English) */
+$magicWords['en'] = array(
+	'autoincrement' => array( 0, 'AUTOINCREMENT' ),
+);
+
+/** Arabic (العربية) */
+$magicWords['ar'] = array(
+	'autoincrement' => array( 0, 'زيادة_تلقائية' ),
+);
+
+/** Egyptian Spoken Arabic (مصرى) */
+$magicWords['arz'] = array(
+	'autoincrement' => array( 0, 'زيادة_تلقائية', 'AUTOINCREMENT' ),
+);
+
+/** Chechen (нохчийн) */
+$magicWords['ce'] = array(
+	'autoincrement' => array( 0, 'ШАЙОККХОЙО', 'АВТОИНКРЕМЕНТ', 'AUTOINCREMENT' ),
+);
+
+/** German (Deutsch) */
+$magicWords['de'] = array(
+	'autoincrement' => array( 0, 'AUTOZÄHLUNG' ),
+);
+
+/** Zazaki (Zazaki) */
+$magicWords['diq'] = array(
+	'autoincrement' => array( 0, 'OTOMATİKZEYDNAYIŞ' ),
+);
+
+/** Spanish (español) */
+$magicWords['es'] = array(
+	'autoincrement' => array( 0, 'AUTOINCREMENTO', 'AUTOINCREMENTAR' ),
+);
+
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'autoincrement' => array( 0, '自動インクリメント', 'オートインクリメント' ),
+);
+
+/** Korean (한국어) */
+$magicWords['ko'] = array(
+	'autoincrement' => array( 0, '번호' ),
+);
+
+/** Macedonian (македонски) */
+$magicWords['mk'] = array(
+	'autoincrement' => array( 0, 'АВТОРАСТ' ),
+);
+
+/** Malayalam (മലയാളം) */
+$magicWords['ml'] = array(
+	'autoincrement' => array( 0, 'സ്വയംകൂടുക' ),
+);
+
+/** Marathi (मराठी) */
+$magicWords['mr'] = array(
+	'autoincrement' => array( 0, 'स्वयंवाढ', 'AUTOINCREMENT' ),
+);
+
+/** Nedersaksisch (Nedersaksisch) */
+$magicWords['nds-nl'] = array(
+	'autoincrement' => array( 0, 'AUTEMATIESOPHOGEN', 'AUTOMATISCHOPHOGEN' ),
+);
+
+/** Dutch (Nederlands) */
+$magicWords['nl'] = array(
+	'autoincrement' => array( 0, 'AUTOMATISCHOPHOGEN' ),
+);
+
+/** Russian (русский) */
+$magicWords['ru'] = array(
+	'autoincrement' => array( 0, 'АВТОИНКРЕМЕНТ' ),
+);
+
+/** Serbian (Cyrillic script) (српски (ћирилица)‎) */
+$magicWords['sr-ec'] = array(
+	'autoincrement' => array( 0, 'САМОУВЕЋАЊЕ' ),
+);
+
+/** Serbian (Latin script) (srpski (latinica)‎) */
+$magicWords['sr-el'] = array(
+	'autoincrement' => array( 0, 'SAMOUVEĆAVANJE' ),
+);
+
+/** Swedish (svenska) */
+$magicWords['sv'] = array(
+	'autoincrement' => array( 0, 'ÖKAAUTOMATISKT', 'AUTOINCREMENT' ),
+);
+
+/** Turkish (Türkçe) */
+$magicWords['tr'] = array(
+	'autoincrement' => array( 0, 'OTOARTIR' ),
+);
+
+/** Ukrainian (українська) */
+$magicWords['uk'] = array(
+	'autoincrement' => array( 0, 'АВТОІНКРЕМЕНТ' ),
+);
